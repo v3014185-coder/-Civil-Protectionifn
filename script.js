a7f1f21a0714c715cace8f8d0b3cec7ab09a34b3
ELITE INF <v3014185@gmail.com>
2026-07-04 00:09:57 -0700

    script.js

--- script.js
+++ script.js
@@ -2,26 +2,43 @@
 const startMenu = document.getElementById('start-menu');
 const buttonsMenu = document.getElementById('buttons-menu');
 const textMenu = document.getElementById('text-menu');
+const rulesMenu = document.getElementById('rules-menu');
 
-// Кнопки
+// Кнопки переходов
 const activateBtn = document.getElementById('activate-btn');
 const btnWho = document.getElementById('btn-who');
-const btnBack = document.getElementById('btn-back');
+const btnRules = document.getElementById('btn-rules');
 
-// 1. Нажатие на "АКТИВИРОВАТЬ" -> открывает меню с тремя кнопками
+// Кнопки "Назад"
+const btnBackWho = document.getElementById('btn-back-who');
+const btnBackRules = document.getElementById('btn-back-rules');
+
+// 1. АКТИВИРОВАТЬ -> открывает главное меню
 activateBtn.addEventListener('click', () => {
     startMenu.classList.add('hidden');
     buttonsMenu.classList.remove('hidden');
 });
 
-// 2. Нажатие на первую кнопку "Кто такие ELITE?" -> открывает текст
+// 2. Кнопка "Кто такие ELITE?" -> открывает первый текст
 btnWho.addEventListener('click', () => {
     buttonsMenu.classList.add('hidden');
     textMenu.classList.remove('hidden');
 });
 
-// 3. Нажатие на кнопку "НАЗАД" -> возвращает из текста к трем кнопкам
-btnBack.addEventListener('click', () => {
+// 3. Кнопка "Где взять устав..." -> открывает экран с уставом и ссылкой
+btnRules.addEventListener('click', () => {
+    buttonsMenu.classList.add('hidden');
+    rulesMenu.classList.remove('hidden');
+});
+
+// 4. Кнопка "Назад" из меню "Кто такие" -> возвращает в главное меню
+btnBackWho.addEventListener('click', () => {
     textMenu.classList.add('hidden');
+    buttonsMenu.github.io / buttonsMenu.classList.remove('hidden');
+});
+
+// 5. Кнопка "Назад" из меню Устава -> возвращает в главное меню
+btnBackRules.addEventListener('click', () => {
+    rulesMenu.classList.add('hidden');
     buttonsMenu.classList.remove('hidden');
 });
